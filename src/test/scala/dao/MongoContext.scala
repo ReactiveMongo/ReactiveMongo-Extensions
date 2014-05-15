74aa32b528d373a4bba6f1264cb684a9f70cc57a
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2014-05-15 23:53:04 +0300

    Remove unnecessary code in MongoContext

--- src/test/scala/dao/MongoContext.scala
+++ src/test/scala/dao/MongoContext.scala
@@ -22,5 +22,5 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object MongoContext {
   val driver = new MongoDriver
   val connection = driver.connection(List("localhost"))
-  def db: () => DB = () => connection("test-reactivemongo-extensions")
+  def db(): DB = connection("test-reactivemongo-extensions")
 }
