22feeec68a9ac5b2b7498a15fca6618e4f4925b1
Fehmi Can Sağlam <fehmican.saglam@gmail.com>
2014-08-09 22:20:15 +0300

    Update publishSnapshots.sh

--- .travis_scripts/publishSnapshots.sh
+++ .travis_scripts/publishSnapshots.sh
@@ -2,12 +2,12 @@
 
 echo -e "Starting publish to Sonatype...\n"
 
-./sbt ";project bson;publishSnapshotsFromTravis ;project core;publishSnapshotsFromTravis ;project json;publishFromTravis"
+sbt ";project bson;publishSnapshotsFromTravis ;project core;publishSnapshotsFromTravis ;project json;publishFromTravis"
 RETVAL=$?
 
 if [ $RETVAL -eq 0 ]; then
   echo "Snapshots successfully published"
 else
   echo "Error while publishing snapshots"
-  return 1
+  exit 1
 fi
