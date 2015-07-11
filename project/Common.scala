2766f80f22e127d79cb29d7dfd7bfb070cab504b
cchantep <chantepie@altern.org>
2015-07-11 20:07:23 +0200

    Bump dependency + Maven badge

--- project/Common.scala
+++ project/Common.scala
@@ -12,6 +12,6 @@ object Common {
   }
 
   val playVersion = "2.4.0"
-  val reactiveMongoVersion = "0.11.1"
-  val playReactiveMongoVersion = "0.11.1.play24"
+  val reactiveMongoVersion = "0.11.2"
+  val playReactiveMongoVersion = "0.11.2.play24"
 }
